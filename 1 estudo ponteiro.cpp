eefd2b2d222d7adcc575fd3d856d988c71053820
FernandoJoseAlvesPatriota <fernando.j.a.patriota@gmail.com>
2017-06-15 15:08:38 -0300

    ponteiro primeiro exemplo
     ponteiro primeiro exemplo

---
+++ 1 estudo ponteiro.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main (void) {
+	
+	int x=10;
+	int*ponteiro;
+	ponteiro= &x;
+	
+	int y = 20;
+	*ponteiro = y;
+	
+	
+	 printf("%i %i \n", x, y);
+	 getchar();
+	  return 0;
+	  
+	
+	
+	
+	
+}
